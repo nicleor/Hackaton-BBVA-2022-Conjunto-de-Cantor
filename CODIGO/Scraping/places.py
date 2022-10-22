0e9811a314461e644534e423d46addb7a4c0cbed
jon <34549011+jocaz@users.noreply.github.com>
2022-10-22 17:21:57 -0500

    avances y dummy generator python program

---
+++ CODIGO/Scraping/places.py
@@ -0,0 +1,22 @@
+import json
+
+data = {
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 35,
+    "children": [
+        {
+            "firstName": "Alice",
+            "age": 6
+        },
+        {
+            "firstName": "Bob",
+            "age": 8
+        }
+    ]
+}
+
+with open("data_file.json","w") as file:
+	json.dump(data,file)
+	
