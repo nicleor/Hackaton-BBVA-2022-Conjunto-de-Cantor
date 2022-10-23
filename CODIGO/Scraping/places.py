22be5107d071b4266113de5ad7957e7708237b4f
jon <34549011+jocaz@users.noreply.github.com>
2022-10-23 02:39:24 -0500

    se agrega validacion para status ok y resultados para empresas cercanas

--- CODIGO/Scraping/places.py
+++ CODIGO/Scraping/places.py
@@ -19,6 +19,9 @@ df = pd.read_csv(PATH+'Final_Data_Hackathon_2022.csv')
 csv = pd.read_csv(PATH+'lista_coincidencias_DINUE.csv')
 csv_10 = csv.iloc[0:11:1]
 
+#cercanas
+cercanas = pd.read_csv(PATH+)
+
 #funciones
 def places(busqueda):
     places_result_x = gmaps.places(busqueda)
@@ -56,14 +59,53 @@ resultados = itera_busquedas(searches_1, csv_10, 11)
 #construccion del output
 output = dict()
 for k in resultados.keys():
-    dic_aun = {ka:resultados[k][0]['results'][0][ka] for ka in resultados[k][0]['results'][0] if ka in ['types','user_ratings_total','rating','name','formatted_address','business_status']}
-    dic_aun2 = {ka:resultados[k][1]['result'][ka] for ka in resultados[k][1]['result'] if ka in ['opening_hours','formatted_phone_number','reference','reviews','types','vicinity','website']}
-    if 'reviews' in dic_aun2.keys():
-        for rev in range(len(dic_aun2['reviews'])):
-            texto = dic_aun2['reviews'][rev]['text']
-            dic_aun2['reviews'][rev]['sentiment'] = sia().polarity_scores(texto)['compound']
-    output[k] = [dic_aun,dic_aun2]
+    if resultados[k][0]['status'] == 'OK':
+        dic_aun = {ka:resultados[k][0]['results'][0][ka] for ka in resultados[k][0]['results'][0] if ka in ['types','user_ratings_total','rating','name','formatted_address','business_status']}
+        dic_aun2 = {ka:resultados[k][1]['result'][ka] for ka in resultados[k][1]['result'] if ka in ['formatted_phone_number','reference','reviews','types','vicinity','website']}
+        if 'reviews' in dic_aun2.keys():
+            for rev in range(len(dic_aun2['reviews'])):
+                texto = dic_aun2['reviews'][rev]['text']
+                dic_aun2['reviews'][rev]['sentiment'] = sia().polarity_scores(texto)['compound']
+        if 'opening_hours' in resultados[k][1]['result'].keys():
+            dic_aun2['weekday_text'] = resultados[k][1]['result']['opening_hours']['weekday_text']
+        output[k] = [dic_aun,dic_aun2]
+    else:
+        output[k] = [dict(),dict()]
 
 #creando el json de salida
 with open(PATH+'output.json','w') as f:
     json.dump(output,f)
+
+
+#empresas cercanas
+cercanas = pd.read_csv(PATH+'Empresas_cercanas.csv',index_col=0, encoding='latin-1')
+
+#Nombre Colonia localidad.y entidad
+cercanas['search'] = cercanas['Nombre']+', '+cercanas['localidad.y']+', '+cercanas['entidad']
+close_search = list(cercanas['search'])
+
+#resultados de la busqueda de empresas cercanas
+results_close = dict()
+for c in close_search:
+    results_close[c] = list(places(c))
+
+#creando objeto de salida
+output_c = dict()
+for k in results_close.keys():
+    if results_close[k][0]['status'] == 'OK':
+        dic_aun = {ka:results_close[k][0]['results'][0][ka] for ka in results_close[k][0]['results'][0] if ka in ['types','user_ratings_total','rating','name','formatted_address','business_status']}
+        dic_aun2 = {ka:results_close[k][1]['result'][ka] for ka in results_close[k][1]['result'] if ka in ['formatted_phone_number','reference','reviews','types','vicinity','website']}
+        if 'reviews' in dic_aun2.keys():
+            for rev in range(len(dic_aun2['reviews'])):
+                texto = dic_aun2['reviews'][rev]['text']
+                dic_aun2['reviews'][rev]['sentiment'] = sia().polarity_scores(texto)['compound']
+        if 'opening_hours' in results_close[k][1]['result'].keys():
+            dic_aun2['weekday_text'] = results_close[k][1]['result']['opening_hours']['weekday_text']
+        output_c[k] = [dic_aun,dic_aun2]
+    else:
+        output_c[k] = [dict(),dict()]
+
+#creando el json de salida para empresas cercanas
+with open(PATH+'output_cercanos.json','w') as f:
+    json.dump(output_c,f)
+
